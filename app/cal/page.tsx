de4191733169ebf9ba49a3963c9732ad3bfa094c
mdmasum <mdmasumpresent@gmail.com>
2026-03-19 21:12:13 +0600

    ready normal calculator

--- app/cal/page.tsx
+++ app/cal/page.tsx
@@ -5,11 +5,13 @@ import { useRef, useState } from "react";
 const Cal = () => {
 
     const [status,setStatus] = useState<string>('');
-    const [result,setResult] = useState<string>('0');
+    const [result,setResult] = useState<string>('');
     const [input1,setInput1] = useState<string>('');
     const [input2,setInput2] = useState<string>('');
     const [active,setActive] = useState<number>(0);
     const timer = useRef<NodeJS.Timeout | null>(null)
+    const [left,setLeft] = useState<boolean>(false)
+    const [right,setRight] = useState<boolean>(false)
     
     const addValue = (num: string) => {
         // const regEx = /^[^.]*\.[^.]*$/;
@@ -17,16 +19,25 @@ const Cal = () => {
             setActive(1)
         }
         if (active === 1){
+            setLeft(true)
+            setRight(false)
             setInput1(prev => prev + num)
         }else if (active === 2){
+            setRight(true)
+            setLeft(false)
             setInput2(prev => prev + num)
         }
     }
     
     const c = () => {
+        setLeft(true)
+        setRight(false)
+        setActive(1)
         setInput1('')
         setInput2('')
-        setResult('0')
+        setResult('')
+        setStatus('')
+
     }
     const deleteLastDigit = () => {
         if (active === 1){
@@ -46,14 +57,18 @@ const Cal = () => {
     const addition = () => {
         let value1 = parseFloat(input1)
         let value2 = parseFloat(input2)
-        setResult(String(value1+value2))
+        let calculate = value1 + value2
+        setStatus('+')
+        setResult(String(calculate.toFixed(2)))
     }
     ///////////////////////////////////////////
     ///////////////////////////////////////////
     const subtraction = () => {
         let value1 = parseFloat(input1)
         let value2 = parseFloat(input2)
-        setResult(String(value1-value2))
+        let calculate = value1 - value2
+        setStatus('-')
+        setResult(String(calculate.toFixed(2)))
         
     }
     ///////////////////////////////////////////
@@ -61,14 +76,55 @@ const Cal = () => {
     const multi = () => {
         let value1 = parseFloat(input1)
         let value2 = parseFloat(input2)
-        setResult(String(value1*value2))
+        let calculate = value1 * value2
+        setStatus('x')
+        setResult(String(calculate.toFixed(2)))
     }
     ///////////////////////////////////////////
+    const leftFocus = () => {
+        setActive(1)
+        setLeft(true)
+        setRight(false)
+    }
+    const rightFocus = () => {
+        setActive(2)
+        setRight(true)
+        setLeft(false)
+    }
     ///////////////////////////////////////////
     const divition = () => {
         let value1 = parseFloat(input1)
         let value2 = parseFloat(input2)
-        setResult(String(value1/value2))
+        let calculate = value1 / value2
+        setStatus('÷')
+        setResult(String(calculate.toFixed(2)))
+    }
+    ///////////////////////////////////////////
+    const leftHandle = () => {
+        setActive(1)
+        setLeft(true)
+        setRight(false)
+    }
+    const rightHandle = () => {
+        setActive(2)
+        setLeft(false)
+        setRight(true)
+    }
+    ///////////////////////////////////////////
+    const serial = () => {
+        console.log('Changed!!!')
+    }
+    ///////////////////////////////////////////
+    const resultHandle = () => {
+        if (result === ''){
+        }else{
+            setInput1(result)
+            setResult('')
+            setInput2('')
+            setLeft(false)
+            setRight(true)
+            setStatus('')
+        }
     }
     ///////////////////////////////////////////
     return(
@@ -82,18 +138,20 @@ const Cal = () => {
             </div> */}
             {/* //////////////////////////////////////////////////// */}
             <div className="section flex bg-black text-white rounded-xl py-2 px-3 justify-center mt-10">
-                <input value={input1} onFocus={() => setActive(1)} type="text" placeholder="Select 1" required className="text-center text-xl w-35 py-1 rounded-xl" readOnly/>
+                <input value={input1} onFocus={leftFocus} type="text" placeholder="input 1" required className="text-center text-xl w-35 py-1 rounded-xl" readOnly/>
 
                 <h1 className="text-3xl ml-3 mr-3">{status}</h1>
 
-                <input value={input2} onFocus={() => setActive(2)} type="text" placeholder="Select 2" required className="text-center text-xl w-35 py-1 rounded-xl" readOnly/>
+                <input value={input2} onFocus={rightFocus} type="text" placeholder="input 2" required className="text-center text-xl w-35 py-1 rounded-xl" readOnly/>
             </div>
-            <h1 className="text-center text-2xl mt-3 font-bold">Result = <span className="font-bold text-white bg-amber-800 py-1 px-3 rounded-xl">{result}</span></h1>
+            <div>
+                <h1 className="text-center text-2xl mt-3 font-bold">Result = <span className="font-bold text-white bg-amber-800 py-1 px-3 rounded-xl ml-2">{result}</span><h1 className="fa fa-check bg-green-500 py-1 rounded-xl ml-3 text-white border-2 border-amber-500 pl-2 pr-7 cursor-pointer hover:bg-green-600" onClick={resultHandle}></h1></h1>
+            </div>
             {/* //////////////////////////////////////////////////// */}
             <div className="flex mt-15 items-center justify-center gap-20 pb-5">
-                <h1 className="fa fa-arrow-left bg-green-500 text-3xl py-1 rounded-lg pl-3 pr-12 text-white border-2 border-red-800" onClick={()=> setActive(1)}></h1>
+                <h1 className={left ? "fa fa-arrow-left bg-green-500 text-3xl py-1 rounded-lg pl-3 pr-12 text-white border-2 border-red-800 opacity-50": "fa fa-arrow-left bg-green-500 text-3xl py-1 rounded-lg pl-3 pr-12 text-white border-2 border-red-800"} onClick={leftHandle}></h1>
                 
-                <h1 className="fa fa-arrow-right bg-green-500 text-3xl py-1 rounded-lg pl-3 pr-12 text-white border-2 border-red-800" onClick={() => setActive(2)}></h1>
+                <h1 className={right ? "fa fa-arrow-right bg-green-500 text-3xl py-1 rounded-lg pl-3 pr-12 text-white border-2 border-red-800 opacity-50" : "fa fa-arrow-right bg-green-500 text-3xl py-1 rounded-lg pl-3 pr-12 text-white border-2 border-red-800"} onClick={rightHandle}></h1>
                 
             </div>
             <div className="grid grid-cols-4 gap-5 bg-emerald-800 py-8 px-5 bottom-0 right-0 rounded-xl border-3 border-fuchsia-700">
--- app/layout.tsx
+++ app/layout.tsx
@@ -32,9 +32,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <nav className="flex items-center gap-5 bg-green-600 py-4 px-8 border-b-5 border-black">
+        <nav className="flex items-center text-center gap-5 bg-green-600 py-4 px-8 border-b-5 border-black">
           <Image src={logo} height={40} alt="Logo-Image" className="bg-white rounded-full p-1 animate-[spin_4s_infinite]"></Image>
           <h1 className="text-2xl font-bold text-white">Calculator</h1>
+          <p className="text-center font-bold font-mono text-sm text-amber-300">Masum Software Lab</p>
         </nav>
 
         {children}
